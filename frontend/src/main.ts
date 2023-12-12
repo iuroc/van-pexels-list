8b2026ac671b411361b65ea404696b766c51d46b
oyps <ponconsoft@gmail.com>
2023-12-13 06:30:27 +0800

    ...

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,76 +1,107 @@
 /// <reference types="vite/client" />
 import van, { State } from 'vanjs-core'
-import '../scss/main.sass'
+import { Photo, getPhotos, loadPage, srcDesc } from './mixin'
+import { Tooltip } from 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
 
-const { button, textarea } = van.tags
-
-interface Line {
-    /** 当前行序号 */
-    index: number
-    /** 当前行文本 */
-    text: string
-    /** 光标在当前行的位置 */
-    cursor: number
-}
-
-type GetLine = (text: string, position: number) => Line
-/** 获取当前光标所在行数 */
-const getLine: GetLine = (text, position) => {
-    const lines: string[] = text.split('\n')
-    for (let num = 0, i = 0; i < lines.length; i++) {
-        let line: string = lines[i]
-        num += line.length + 1
-        if (position < num) return {
-            index: i,
-            text: line,
-            cursor: line.length - num + position + 1
-        }
-    }
-    return { index: 0, text: '', cursor: 0 }
-}
-
-const ifCursorAtSpace = (line: Line) => {
-    const result = line.text.match(/^(\s*)/) as RegExpMatchArray
-    return line.cursor <= result[1].length
-}
-
-const CodeEditor = (text: State<string>) => {
-    return textarea({
-        style: `
-      width: 100%;
-      outline: none;
-      font-size: 20px;
-      font-family: consolas;
-      `,
-        rows: 10,
-        onkeydown(event) {
-            if (event.key == 'Tab') {
-                event.preventDefault()
-                const position: number = event.target.selectionStart
-                const positionEnd: number = event.target.selectionEnd
-                const line = getLine(text.val, position)
-                if (ifCursorAtSpace(line)) {
-                    let frontText = text.val.substring(0, position)
-                    let endText = text.val.substring(position)
-                    ele.value = frontText + '    ' + endText
-                    event.target.selectionStart = position + 4
-                    event.target.selectionEnd = positionEnd + 4
+const { a, div, h3, li, nav, table, tbody, td, th, thead, tr, ul } = van.tags
+const Pagination = (activePage: State<number>, total: State<number>, click: (page: number) => void) => {
+    const Item = (text: string, page: number) => {
+        return li({ class: 'page-item' },
+            a({
+                href: '#',
+                class: () => `page-link ${page == activePage.val ? 'active' : ''}`, onclick(event: Event) {
+                    event.preventDefault()
+                    if (page != -1) {
+                        click(page)
+                        activePage.val = page
+                    }
                 }
-            }
-        },
-        oninput(event) {
-            text.val = event.target.value
-        },
-        value: text
-    })
-}
-const text = van.state(`public static void main(String[] args) {
-  System.out.println("Hello World");
-}`)
-
-const ele = CodeEditor(text)
-van.add(document.body, ele, button({
-    onclick() {
-        text.val = ''
+            }, text),
+        )
     }
-}, '清空'))
+    const pageData = van.derive(() => loadPage(activePage.val, total.val))
+    return nav(
+        () => ul({ class: 'pagination' },
+            pageData.val.prev == 0 ? '' : Item('上一页', pageData.val.prev),
+            pageData.val.pages.map(page => Item(page == -1 ? '...' : page.toString(), page)),
+            pageData.val.next == 0 ? '' : Item('下一页', pageData.val.next),
+        ),
+    )
+}
+const App = () => {
+    const tableBody = tbody()
+    const keys: (keyof Photo)[] = ['id', 'width', 'height', 'photographer', 'alt', 'src']
+    const activePage = van.state(1)
+    const totalPage = van.state(0)
+    const pageLoading = van.state(true)
+    const loadPhotos = (pageIndex: number) => {
+        getPhotos(pageIndex, 100).then(data => {
+            const { photos, pageSize } = data
+            totalPage.val = Math.ceil(8000 / pageSize)
+            photos.forEach(photo => {
+                van.add(tableBody, tr(
+                    keys.map(key => {
+                        if (key == 'src') {
+                            return Object.keys(photo.src).map((srcKey) => {
+                                const ele = a({
+                                    href: photo.src[srcKey as keyof Photo['src']],
+                                    target: '_blank',
+                                    'data-bs-toggle': 'tooltip',
+                                    'data-bs-title': srcDesc[srcKey as keyof Photo['src']]
+                                }, srcKey)
+                                new Tooltip(ele)
+                                return td(ele)
+                            })
+                        } else if (key == 'width' || key == 'height') {
+                            return td(photo[key], 'px')
+                        } else if (key == 'photographer') {
+                            return td(a({ href: photo.photographerUrl, class: 'link-secondary', target: '_blank' }, photo.photographer))
+                        } else if (key == 'id') {
+                            return th({ scope: 'row' }, a({ href: photo.url, class: 'link-secondary', target: '_blank' }, photo.id))
+                        } else if (key == 'alt') {
+                            const ele = a({
+                                href: '#', onclick(event: Event) { event.preventDefault() },
+                                class: 'link-secondary',
+                                'data-bs-toggle': 'tooltip',
+                                'data-bs-placement': 'left',
+                                'data-bs-title': photo.alt || '暂无描述'
+                            }, '查看描述')
+                            new Tooltip(ele)
+                            return td(ele)
+                        } else {
+                            return td(photo[key] as string)
+                        }
+                    })
+                ))
+            })
+            pageLoading.val = false
+        })
+    }
+    loadPhotos(0)
+
+    return div({ class: 'container py-4' },
+        h3({ class: 'mb-4' }, '8000 张 Pexels 图片'),
+        div({ class: 'overflow-x-auto mb-4' },
+            table({ class: 'mb-0 table table-striped table-bordered text-center text-nowrap' },
+                thead(
+                    tr(
+                        ['图片编号', '宽度', '高度', '摄影师', '图片描述'].map(text => th({ scope: 'col' }, text)),
+                        th({ colSpan: 8 }, '图片文件')
+                    ),
+                ),
+                tableBody
+            )
+        ),
+        div({ hidden: pageLoading },
+            Pagination(activePage, totalPage, page => {
+                tableBody.innerHTML = ''
+                pageLoading.val = true
+                setTimeout(() => {
+                    loadPhotos(page - 1)
+                }, 100)
+            })
+        )
+    )
+}
+van.add(document.body, App())
--- frontend/src/mixin.ts
+++ frontend/src/mixin.ts
@@ -1,45 +1,84 @@
 /* 表示具有各种属性的照片 */
 export interface Photo {
     /** 照片的标识号 */
-    id: number;
+    id: number
     /** 照片的实际宽度（以像素为单位） */
-    width: number;
+    width: number
     /** 照片的实际高度（以像素为单位） */
-    height: number;
+    height: number
     /** 存放照片的 Pexels URL */
-    url: string;
+    url: string
     /** 拍摄照片的摄影师的姓名 */
-    photographer: string;
+    photographer: string
     /** 拍摄照片的摄影师的 Pexels 个人资料 URL */
-    photographerUrl: string;
+    photographerUrl: string
     /** 摄影师的标识号 */
-    photographerId: number;
+    photographerId: number
     /** 照片的平均颜色在图像加载时用作占位符 */
-    avgColor: string;
+    avgColor: string
     /** 表示照片是否被喜欢 */
-    liked: string;
+    liked: string
     /** 用于 alt 属性的照片的文本描述 */
-    alt: string;
+    alt: string
     /** 用于显示此照片的不同图像大小的集合 */
-    src: SrcType;
+    src: Record<keyof typeof srcDesc, string>
 }
 
-/* 表示用于显示照片的不同图像大小 */
-export interface SrcType {
-    /** 没有任何大小更改的图像与宽度和高度属性相同 */
-    original: string;
-    /** 调整为 W 940px X H 650px DPR 1 的图像 */
-    large2x: string;
-    /** 调整为 W 940px X H 650px DPR 2 的图像 */
-    large: string;
-    /** 等比例缩放，使其新高度为 350px 的图像 */
-    medium: string;
-    /** 等比例缩放，使其新高度为 130px 的图像 */
-    small: string;
-    /** 裁剪为 W 800px X H 1200px 的图像 */
-    portrait: string;
-    /** 裁剪为 W 1200px X H 627px 的图像 */
-    landscape: string;
-    /** 裁剪为 W 280px X H 200px 的图像 */
-    tiny: string;
+export const getPhotos = (page: number = 0, pageSize: number = 36) => {
+    return new Promise<{
+        photos: Photo[],
+        page: number,
+        pageSize: number
+    }>(resolve => {
+        const params = new URLSearchParams()
+        params.set('page', page.toString())
+        params.set('pageSize', pageSize.toString())
+        fetch('/api/photos?' + params.toString()).then(res => {
+            res.json().then(photos => {
+                resolve({ photos, page, pageSize })
+            })
+        })
+    })
+}
+
+/** 图片文件类型描述 */
+export const srcDesc = {
+    original: '原始图片',
+    large2x: '宽 940px 高 650px DPR 2',
+    large: '宽 940px 高 650px DPR 1',
+    medium: '等比例缩放，高 350px',
+    small: '等比例缩放，高 130px',
+    portrait: '宽 800px 高 1200px',
+    landscape: '宽 1200px 高 627px',
+    tiny: '宽 280px 高 200px'
+}
+
+/** 获取翻页器数据组合，-1 表示省略，0 表示隐藏 */
+export const loadPage = (page: number, total: number) => {
+    /*
+    1. 总页码 > 7 时，前 4 页的排列相同，后 4 页的排列相同
+        前4页：[1] [2] [3] [4] [5] ... [-1]
+        中间：[1] ... [p-1] [p] [p+1] ... [-1]
+        后4页：[1] ... [-5] [-4] [-3] [-2] [-1]
+    2. 总页码 <= 7 时，只有 1 种排列
+    */
+    type Page = number
+    const pages: Page[] = []
+    if (total > 7) {
+        if (page < 5) {
+            pages.push(...[1, 2, 3, 4, 5, -1, total])
+        } else if (total - page < 4) {
+            pages.push(...[1, -1, total - 4, total - 3, total - 2, total - 1, total])
+        } else {
+            pages.push(...[1, -1, page - 1, page, page + 1, -1, total])
+        }
+    } else {
+        for (let i = 0; i < 7; i++)
+            pages.push(i + 1)
+    }
+    return {
+        prev: page <= 1 ? 0 : page - 1,
+        next: page >= total ? 0 : page + 1,
+        pages
+    }
 }
